5b48010126481d9a8e6cc7d969d467db9b6ef02a
Myada Roshdi <roshdy.miada@gmail.com>
2018-05-26 15:48:02 -0400

    error fix2

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -192,8 +192,8 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 	// The upper and lower limits of delta are set to -25 and 25
 	// degrees (values in radians).
 	for (int i = delta_start; i < a_start; i++) {
-		vars_lowerbound[i] = -0.436332;
-		vars_upperbound[i] = 0.436332;
+		vars_lowerbound[i] = -0.436332 * Lf;
+		vars_upperbound[i] = 0.436332 * Lf;
 	}
 
 	// Acceleration/decceleration upper and lower limits.
