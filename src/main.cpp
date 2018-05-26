b3f1079b0ccbc0a91da8bb2fde274f503c117184
Myada Roshdi <roshdy.miada@gmail.com>
2018-05-26 15:30:30 -0400

    functions implemented without Latency

--- src/main.cpp
+++ src/main.cpp
@@ -85,12 +85,51 @@ int main() {
         string event = j[0].get<string>();
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          vector<double> ptsx = j[1]["ptsx"];
-          vector<double> ptsy = j[1]["ptsy"];
-          double px = j[1]["x"];
-          double py = j[1]["y"];
-          double psi = j[1]["psi"];
-          double v = j[1]["speed"];
+		 // what we get from the simulator:
+          vector<double> ptsx = j[1]["ptsx"];// The global x positions of the waypoints.
+          vector<double> ptsy = j[1]["ptsy"];//The global y positions of the waypoints. This corresponds to the z coordinate in Unity since y is the up-down direction.
+          double px = j[1]["x"];//The global x position of the vehicle.
+          double py = j[1]["y"];//The global y position of the vehicle.
+          double psi = j[1]["psi"];//The orientation of the vehicle in radians converted from the Unity format to the standard format expected in most mathemetical functions
+          double v = j[1]["speed"];// The current velocity in mph.
+		  double delta = j[1]["steering_angle"];//The current steering angle in radians.
+		  double a = j[1]["throttle"];//The current throttle value [-1, 1].
+
+
+		// Transform all the points from simulator (global Cooridinates)to the vehicle's orientation
+		  for (int i = 0; i < ptsx.size(); i++) {
+			  //Shift car reference angle to 90-degree
+			  double x = ptsx[i] - px;
+			  double y = ptsy[i] - py;
+			  ptsx_car[i] = x * cos(-psi) - y * sin(-psi);
+			  ptsy_car[i] = x * sin(-psi) + y * cos(-psi);
+		  }
+
+		  // Transrom the points vectors to Eigen vectors for polyfit in car Coordinates
+		  Eigen::VectorXd ptsx_transform(ptsx.size()); 
+		  Eigen::VectorXd ptsy_transform(ptsy.size());
+
+		  // Fitting the transformed points to  a 3rd - order polynomial
+		  auto coeffs = polyfit(ptsx_transform, ptsy_transform, 3);
+
+	      // Calculate Initial cte, now transformed points are in vehicle coordinates, so x & y equal 0 below.
+		  // otherwise y shoud be subtracted from the polyeval value
+		  double cte = polyeval(coeffs, 0); // Our target: it to turn cte =0
+
+		  // Calculate the Initial orientation error
+		 // double epsi = psi - -atan(coeffs[1]+ 2 * px * coeffs[2] + 3 * coeffs[3] * pow(px,2))
+		  // Since initially  x = 0 in the vehicle coordinates, so the higher orders are zero and also psi=0 (initially)
+	
+
+		  // double epsi = psi - -atan(coeffs[1]+ 2 * px * coeffs[2] + 3 * coeffs[3] * pow(px,2))
+		  double epsi = -atan(coeffs[1]); // simplification of the above equation of epsi, and our target: is to turn epsi = 0
+
+		// Initial state values (in car coordinates)
+		  Eigen::VectorXd state(6);
+		  state << 0, 0, 0, v, cte, epsi;//  vector state -->[ x,y,psi,v,cte,epsi]
+
+
+
 
           /*
           * TODO: Calculate steering angle and throttle using MPC.
@@ -98,32 +137,52 @@ int main() {
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+
+		  // Call Solve for new actuations (and to show predicted x and y in the future)
+		  auto vars = mpc.Solve(state, coeffs);
+
+
+		  // FOR VISUAL DISPLAY PURPOSE: The next_x and next_y variables display a line projection in yellow (waypoints/reference line) according to polynomial fit.
+		  vector<double> next_x_vals;
+		  vector<double> next_y_vals;
+		  // add (x,y) points to list here, points are in reference to the vehicle's coordinate system
+		  // the points in the simulator are connected by a Yellow line (the line the car is trying to follow)
+		  double poly_inc = 2.5;// distance in x
+		  int num_points = 25; // wanna see 25-points of the future
+
+		  for (int i = 1; i < num_points; i++) {
+			  next_x_vals.push_back(poly_inc * i);
+			  next_y_vals.push_back(polyeval(coeffs, poly_inc * i));
+		  }
+
+
+		  // // FOR VISUAL DISPLAY PURPOSE: The mpc_x_vals and mpc_y_vals variables display a line projection in Green representing the MPC predicted trajectory.
+		  vector<double> mpc_x_vals = { state[0] };
+		  vector<double> mpc_y_vals = { state[1] };
+		  // for vars[], every Even value is added to x and every Odd value is added to y.
+		  for (int i = 2; i < vars.size(); i += 2) {
+			  mpc_x_vals.push_back(vars[i]);
+			  mpc_y_vals.push_back(vars[i + 1]);
+		  }
+
+
+		  // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
+		  // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+          double steer_value = vars[0] / (deg2rad(25) * Lf);;
+          double throttle_value = vars[1];
+
+		  double Lf = 2.67;
 
           json msgJson;
-          // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
-          // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+          
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
-
-          //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals;
-          vector<double> mpc_y_vals;
-
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
-
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
-
-          //Display the waypoints/reference line
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
-
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
-
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
