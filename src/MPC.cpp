8b73ee0e3f9c308ca34ac49e4a5b11acf562ff60
Myada Roshdi <30698281+MyadaRoshdi@users.noreply.github.com>
2018-05-28 06:02:52 -0400

    Update MPC.cpp

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -123,11 +123,11 @@ class FG_eval {
 		  AD<double> delta0 = vars[delta_start + t - 1];
 		  AD<double> a0 = vars[a_start + t - 1];
 		  
-                  /*
+                  
 		  if (t > 1) {   //Use previous actuations (to account for latency)
 			  a0 = vars[a_start + t - 2];
 			  delta0 = vars[delta_start + t - 2];
-		  }*/
+		  }
 
 		  AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * pow(x0, 2) + coeffs[3] * pow(x0, 3);
 		  AD<double> psi_des0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0 + 3 * coeffs[3] * pow(x0, 2)); // Desired psi-value
