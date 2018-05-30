610ebc52f272ff90fe4cf766b57ed876fc73bf57
Myada Roshdi <mroshdi@behrtechnologies.com>
2018-05-30 12:09:54 -0400

    Tuning upper and lower bounds of delta

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -194,8 +194,8 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 	// The upper and lower limits of delta are set to -25 and 25
 	// degrees (values in radians).
 	for (int i = delta_start; i < a_start; i++) {
-		vars_lowerbound[i] = -1.0 ;
-		vars_upperbound[i] =  1.0;
+		vars_lowerbound[i] = -0.436332 ;
+		vars_upperbound[i] =  0.436332;
 	}
 
 	// Acceleration/decceleration upper and lower limits.
